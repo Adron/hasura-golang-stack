e630e1f616abecf2baf37ce267dc4df4b51cc9ae
Adron Hall <adron@hasura.io>
2021-01-19 10:47:40 -0800

    A basic golang stack example using default libraries to execute graphql.

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello world.")
+	jsonData := map[string]string{
+		"query": `{
+		  tododeluge_Todo {
+			Id			
+			Details
+			Todo
+		  }
+		}`,
+	}
+	jsonValue, _ := json.Marshal(jsonData)
+	request, err := http.NewRequest("POST", "https://graphql-go-workflow.hasura.app/v1/graphql", bytes.NewBuffer(jsonValue))
+	client := &http.Client{Timeout: time.Second * 10}
+	response, err := client.Do(request)
+	defer response.Body.Close()
+	if err != nil {
+		fmt.Printf("Failure Failure!: %s \n", err)
+	}
+	data, _ := ioutil.ReadAll(response.Body)
+	fmt.Println(string(data))
 }
